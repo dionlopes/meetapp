da36e9f70e39e5e6abe89380007b525afb654e05
Dion <dion.mello@hotmail.com>
2019-08-13 18:19:45 -0300

    chore: Alterando cor da statusbar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 
-import Routes from './routes';
+import App from './App';
 
-export default function meetapp() {
-  return <Routes />;
+export default function Index() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#22202c" />
+      <App />
+    </>
+  )
 }
